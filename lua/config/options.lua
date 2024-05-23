e4ca57d51addbe73bebf86864388fcbba86f4789
Wang Ziyi <mint.wzy@gmail.com>
2024-05-23 08:46:01 +0800

    enable more languages support and autochdir

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local vim = vim
+
+local options = {
+  autochdir = true,
+}
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
