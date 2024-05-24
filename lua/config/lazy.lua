41b23e93c3c7ad9fb5d0c7c3351b79c6f473de3f
Ziyi Wang <ziyi.wang@autodesk.com>
2024-05-24 18:14:46 +0800

    add some comments

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,7 +28,10 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = {
+    -- try to load one of these colorschemes when starting an installation during startup
+    colorscheme = { "tokyonight", "habamax" },
+  },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
