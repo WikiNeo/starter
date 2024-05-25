9611d4a33b67256220a2d23f0f5c01fc672610e0
Wang Ziyi <mint.wzy@gmail.com>
2024-05-25 09:49:40 +0800

    add Chinese spellcheck support

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,14 +4,17 @@
 
 local vim = vim
 
+-- Set different python3_host_prog value based on OS
 if vim.loop.os_uname().sysname == "Windows_NT" then
   vim.g.python3_host_prog = "C:\\Users\\WZY\\.pyenv-win-venv\\envs\\neovim\\Scripts\\python.exe"
 else
   vim.g.python3_host_prog = "/Users/wangz10/.pyenv/versions/neovim/bin/python"
 end
 
+-- set vim.opt
 local options = {
   autochdir = true,
+  spelllang = { "en", "cjk" },
 }
 
 for k, v in pairs(options) do
