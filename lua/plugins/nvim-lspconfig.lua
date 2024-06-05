9262dadb5843dcaacb92c77b0cdf63235e46c540
Ziyi Wang <ziyi.wang@autodesk.com>
2024-06-05 22:58:14 +0800

    config yamlls

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -3,11 +3,19 @@ return {
   opts = {
     servers = {
       ruff_lsp = {
-        enabled = true,
         init_options = {
           settings = {
             format = {
               args = { "--config=~/.config/ruff.toml" },
+            },
+          },
+        },
+      },
+      yamlls = {
+        settings = {
+          yaml = {
+            editor = {
+              tabSize = 4,
             },
           },
         },
