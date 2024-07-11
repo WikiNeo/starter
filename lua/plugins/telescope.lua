fc861f64a8bd02dc65c08bf9626bcc584c72f568
Ziyi Wang <ziyi.wang@autodesk.com>
2024-07-11 17:05:07 +0800

    udpate telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,12 @@ return {
   "nvim-telescope/telescope.nvim",
   opts = {
     pickers = {
+      find_files = {
+        hidden = true,
+      },
+      grep_string = {
+        additional_args = { "--hidden" },
+      },
       live_grep = {
         additional_args = function(opts)
           return { "--hidden" }
