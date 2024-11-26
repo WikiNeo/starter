cf1423d6a0377d06ca771a78eb74cf9c1d6bc123
Ziyi Wang <ziyi.wang@autodesk.com>
2024-11-26 09:44:29 +0800

    add copilot

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -6,6 +6,7 @@ return {
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
       manual_mode = false,
+      show_hidden = true,
     })
   end,
 }
