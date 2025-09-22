848cd7b8ffced807b22574fbe03e884b198408c3
Ziyi Wang <ziyi.wang@autodesk.com>
2025-09-22 16:27:34 +0800

    update go lsp

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -11,7 +11,7 @@ return {
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
-      manual_mode = false,
+      manual_mode = true,
       show_hidden = true,
     })
   end,
